7f709b197eca491835b3d2a0f484883bff3b23a2
Xinya Zhang <xinyazhang@utexas.edu>
2015-05-12 19:55:53 -0500

    tree/grass: move to the center of camera

--- src/main.cc
+++ src/main.cc
@@ -167,11 +167,11 @@ public:
 		ocean_.reset(new Ocean(32, 5, finalPass_.get()));
 
 		grassFactory_.reset(new GrassFactory(terrain_.get()));
-		grass_.reset(grassFactory_->createGrass(QVector2D(1000, 1000), 1000.0, 40.0, 80.0, 40.0));
+		grass_.reset(grassFactory_->createGrass(QVector2D(cc+1000, cc+1000), 1000.0, 40.0, 80.0, 40.0));
 
 		treeFactory_.reset(new TreeFactory(terrain_.get()));
-		tree1_.reset(treeFactory_->createTree(TreeType::PALM, QVector2D(2000, 2000), 1000.0, 200.0, 140.0, 200.0));
-		tree2_.reset(treeFactory_->createTree(TreeType::TREE1, QVector2D(3100, 3100), 1000.0, 200.0, 140.0, 200.0));
+		tree1_.reset(treeFactory_->createTree(TreeType::PALM, QVector2D(cc+2000, cc+2000), 1000.0, 200.0, 140.0, 200.0));
+		tree2_.reset(treeFactory_->createTree(TreeType::TREE1, QVector2D(cc+3100, cc+3100), 1000.0, 200.0, 140.0, 200.0));
 
 		// Adding objects into reflection pass
 		reflection_->add_light(solar_);
